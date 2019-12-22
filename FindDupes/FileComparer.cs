3d57327c4537f7c78757aaf3d4b416a3f5c7527b
Philip Wong <phil.where@outlook.com>
2019-12-22 13:30:54 +1300

    Add

---
+++ FindDupes/FileComparer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace FindDupes
+{
+    public static class FileComparer
+    {
+        private const int BytesToRead = sizeof(long);
+
+        public static bool FilesAreEqual(FileInfo first, FileInfo second)
+        {
+            if (first.Length != second.Length)
+                return false;
+
+            var iterations = (int)Math.Ceiling((double)first.Length / BytesToRead);
+
+            using (var fs1 = first.OpenRead())
+            using (var fs2 = second.OpenRead())
+            {
+                var one = new byte[BytesToRead];
+                var two = new byte[BytesToRead];
+
+                for (var i = 0; i < iterations; i++)
+                    if (!CompareBytes(fs1, one, fs2, two))
+                        return false;
+            }
+
+            return true;
+        }
+
+        private static bool CompareBytes(FileStream fs1, byte[] one, FileStream fs2, byte[] two)
+        {
+            fs1.Read(one, 0, BytesToRead);
+            fs2.Read(two, 0, BytesToRead);
+
+            return BitConverter.ToInt64(one, 0) == BitConverter.ToInt64(two, 0);
+        }
+    }
+}
---
+++ FindDupes/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FindDupes
+{
+    public class Program
+    {
+        private const string MainDir = "E:\\Other";
+        private static string[] AllDrives = { "E:\\", "C:\\", "T:\\", "S:\\" };
+        private static readonly List<string> ExclusionDirs = new List<string>
+        {
+            "E:\\Prog",
+            "E:\\Music\\Missing Spotify Songs",
+            "C:\\Users\\philw\\Source\\Repos",
+            "C:\\Program Files",
+            "C:\\Windows",
+            "C:\\SteamLibrary",
+            "C:\\Users\\philw\\AppData",
+            "C:\\Users\\All Users",
+            "C:\\ProgramData",
+            "C:\\Users\\philw\\.",
+            "C:\\Users\\philw\\Documents\\Visual Studio",
+            "C:\\Users\\philw\\Desktop\\Code",
+            "C:\\xampp"
+        };
+
+        static void Main(string[] args)
+        {
+            var files = GetFiles(MainDir);
+            var dupes = GetDupes(files);
+            WriteDupesToConsole(dupes);
+            //WriteDvdTitlesToConsole(files);
+            Console.WriteLine("~ fin ~");
+        }
+
+        private static List<FileInfo> GetFiles(params string[] directoryPaths)
+        {
+            var filesPaths = new List<string>();
+            directoryPaths.ToList().ForEach(p => AddFiles(p, filesPaths));
+            var files = filesPaths.Select(f => new FileInfo(f)).ToList();
+            return files;
+        }
+
+        private static void AddFiles(string path, IList<string> files)
+        {
+            try
+            {
+                Directory.GetFiles(path)
+                    .ToList()
+                    .ForEach(files.Add);
+
+                Directory.GetDirectories(path)
+                    .Where(d => !ExclusionDirs.Any(d.StartsWith))
+                    .ToList()
+                    .ForEach(s => AddFiles(s, files));
+            }
+            catch (UnauthorizedAccessException)
+            { }
+        }
+
+        private static void WriteDupesToConsole(IList<IGrouping<long, FileInfo>> dupes)
+        {
+            if (!dupes.Any())
+                return;
+
+            Console.WriteLine("Dupes\n---------------");
+            foreach (var dupe in dupes)
+            {
+                Console.WriteLine($"{dupe.Key}\n---------------");
+                foreach (var fileInfo in dupe)
+                    Console.WriteLine(fileInfo.FullName);
+                Console.WriteLine();
+            }
+        }
+
+        private static IList<IGrouping<long, FileInfo>> GetDupes(IEnumerable<FileInfo> fileInfos)
+        {
+            var dupesBySize = fileInfos.GroupBy(i => i.Length).Where(g => g.Count() > 1).ToList();
+            //dupesBySize.RemoveAll(dupeGroup => !AllExactlyEqual(dupeGroup));
+
+            return dupesBySize;
+        }
+
+        private static bool AllExactlyEqual(IGrouping<long, FileInfo> dupeGroup)
+        {
+            return dupeGroup.All(file =>
+                dupeGroup.All(f => file == f || FileComparer.FilesAreEqual(file, f)));
+        }
+
+        private static void WriteDvdTitlesToConsole(IEnumerable<FileInfo> fileInfos)
+        {
+            var dvdTitles = fileInfos.GroupBy(i => GetDvdMatch(i.Name)).OrderBy(g => g.Key);
+            foreach (var thing in dvdTitles)
+                Console.WriteLine(thing.Key);
+        }
+
+        private static string GetDvdMatch(string fileName)
+        {
+            var parts = fileName.Split(" - ").ToList();
+            var last = parts.IndexOf(parts.Last());
+            return parts.Count > 1 ? parts[last - 1] : "";
+        }
+    }
+}
